8bac522f0e0ad2efa87dd525cca0bc7fffeafc13
OjasJogdand <ojasjogdand31@gmail.com>
2025-01-06 08:11:28 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-
+//Provide your own MongoDB connection string below.
 mongoose.connect("mongodb+srv:///todo_fullstack");
 const todoschema=mongoose.Schema({
     title:String,
